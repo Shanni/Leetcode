beda2ddc638400f276c22e86a8758ffa230e2348
Shan Liu <shan.liu03@gmail.com>
2016-02-06 13:45:31 -0500

    Update numberOfAirplanes.py

--- numberOfAirplanes.py
+++ numberOfAirplanes.py
@@ -1,8 +1,11 @@
-
+"""
+Definition of Interval.
 class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
+"""
+
 
 class Solution:
     # @param airplanes, a list of Interval
@@ -10,11 +13,11 @@ class Solution:
     def countOfAirplanes(self, airplanes):
         # write your code here
         schedule=[()]*(len(airplanes)*2)
-        i=0
-        for start,end in airplanes:
-            schedule[i]=(start,1) 
-            schedule[i+1]=(end,0)
-            i+=2
+
+        for i in range(len(airplanes)):
+            schedule[i*2-1]=(airplanes[i].start,1) 
+            schedule[i*2]=(airplanes[i].end,0)
+
         schedule.sort()
         count=0
         record_max=count
@@ -25,6 +28,5 @@ class Solution:
                 count-=1
             record_max=max(record_max,count)
         return record_max
-
-s=Solution()
-print s.countOfAirplanes([[1,10],[2,3],[5,8],[4,7]])
+                
+            
